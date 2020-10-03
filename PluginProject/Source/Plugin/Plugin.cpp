e7e85e74aae54d718eea0f44bfe1aafb87d1c82a
Alex <alex@codeglue.com>
2020-10-03 12:56:00 +0200

    Revert "Added plugin UE4 project recompiled with UE4.25"
    This reverts commit 2665f179ba20e9e1dc44b60f7af90226dee7a5a1.

--- PluginProject/Source/Plugin/Plugin.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Plugin.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Plugin, "Plugin" );
